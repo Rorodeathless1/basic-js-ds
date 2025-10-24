19a695f3dbc9c05e7e1f1fe483812d8891029de2
Rorodeathless1 <romochka-suvorov-2016@mail.ru>
2025-10-24 12:40:49 +0300

    feat: add tack

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,44 +1,138 @@
 const { NotImplementedError } = require('../lib/errors');
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
+  constructor() {
+    this.rootValue = null;
+  }
+
   root() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    return this.rootValue;
   }
 
-  add(/* data */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  add(value) {
+    const node = new Node(value);
+    if (!this.rootValue) {
+      this.rootValue = node;
+      return;
+    }
+
+    let curNode = this.rootValue;
+    while (curNode) {
+      if (curNode.data === node.data) {
+        return;
+      }
+      if (node.data < curNode.data) {
+        if (!curNode.left) {
+          curNode.left = node;
+          return;
+        }
+        curNode = curNode.left;
+      } else {
+        if (!curNode.right) {
+          curNode.right = node;
+          return;
+        }
+        curNode = curNode.right;
+      }
+    }
   }
 
-  find(/* data */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  has(value) {
+
+    return searchNode(this.rootValue, value);
+
+    function searchNode(node, data) {
+      if (!node) return false;
+
+      if (data === node.data) return true;
+
+      if (data < node.data) {
+        return searchNode(node.left, value);
+      } else {
+        return searchNode(node.right, value);
+      }
+    }
   }
 
-  has(/* data */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  find(value) {
+    return searchNode(this.rootValue, value);
+
+    function searchNode(node, data) {
+      if (!node) return null;
+
+      if (data === node.data) return node;
+
+      if (data < node.data) {
+        return searchNode(node.left, value);
+      } else {
+        return searchNode(node.right, value);
+      }
+    }
   }
 
-  remove(/* data */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  remove(data) {
+    this.rootValue = removeValue(this.rootValue, data);
+
+    function removeValue(node, value) {
+      if (!node) {
+        return null;
+      }
+
+      if (node.data === value) {
+        if (!node.right && !node.left) {
+          return null;
+        } else if (!node.right) {
+          node = node.left;
+          return node;
+        } else if (!node.left) {
+          node = node.right;
+          return node;
+        } else {
+          let maxFromLeft = node.left;
+
+          while (maxFromLeft.right) {
+            maxFromLeft = maxFromLeft.right;
+          }
+
+          node.data = maxFromLeft.data;
+
+          node.left = removeValue(node.left, maxFromLeft.data);
+
+          return node;
+        } 
+      } else if (value < node.data) {
+        node.left = removeValue(node.left, data);
+        return node;
+      } else {
+        node.right = removeValue(node.right, data);
+        return node;
+      }
+    }
   }
 
   min() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    return findMin(this.rootValue);
+
+    function findMin(node) {
+      if (!node.left) return node.data;
+
+      if (node.left) return findMin(node.left);
+    }
   }
 
   max() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    return findMin(this.rootValue);
+
+    function findMin(node) {
+      if (!node.right) return node.data;
+
+      if (node.right) return findMin(node.right);
+    }
   }
 }
 
